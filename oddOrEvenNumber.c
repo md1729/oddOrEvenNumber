11a38344a51bdb0d4854abd88d195c17ddf7b62c
md1729 <muhammet.dilek@metu.edu.tr>
2024-02-15 10:50:04 +0300

    odd or even number in C program
    C Program that calculates whether an entered number is odd or even and prints the result on the screen.

---
+++ oddOrEvenNumber.c
@@ -0,0 +1,14 @@
+
+//Example1: Write a C Program that calculates whether an entered number is odd or even and prints the result on the screen.
+
+#include<stdio.h>
+int main(){
+int num;
+printf("Enter a number:");
+scanf("%d",&num);
+if(num%2==0)
+printf("%d is an even number.\n",num);
+else
+printf("%d is an odd number.\n",num);
+return 0;	
+}
